c9ef5486321aae38f6606330c51a7d82b1d8fca8
kanha-programmer <kanha7017561377@gmail.com>
2026-08-23 22:34:04 +0530

    Time: 2 ms (99.36%), Space: 46.3 MB (88.65%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,27 +1,25 @@
 class Solution {
     public String reverseVowels(String s) {
-      String ans="";
-      int left=0,right=s.length()-1;
-      String vowel="";
-      String vowel2="aeiouAEIOU";
-      for(int i=s.length()-1;i>=0;i--){
-        char x=s.charAt(i);
-        CharSequence cs = String.valueOf(x);
-        if(vowel2.contains(cs)){
-            vowel+=s.charAt(i);
+        char[] ch = s.toCharArray();
+        int left = 0;
+        int right = ch.length - 1;
+        while (left < right) {
+            while (left < right && !isVowel(ch[left])) {
+                left++;
+            }
+            while (left < right && !isVowel(ch[right])) {
+                right--;
+            }
+            char temp = ch[left];
+            ch[left] = ch[right];
+            ch[right] = temp;
+            left++;
+            right--;
         }
-      }
-      int ind=0;
-      for(int i=0;i<s.length();i++){
-        char x=s.charAt(i);
-        CharSequence cs = String.valueOf(x);
-        if(vowel2.contains(cs)){
-            ans+=vowel.charAt(ind);
-            ind++;
-      }else{
-        ans+=s.charAt(i);
-      }
-      }
-      return ans;
+        return new String(ch);
+    }
+    public boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
 }
