85596d4ca6eedb8237bafc9a320e9328028b7bda
kanha-programmer <kanha7017561377@gmail.com>
2026-08-30 12:53:50 +0530

    Time: 2 ms (73.21%), Space: 42.6 MB (80.15%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int j=0,i=0;
+        for(;j<s.length() && i<t.length();){
+            if(s.charAt(j)==t.charAt(i)){
+            j++;
+        }
+        i++;
+    }
+    return j==s.length();
+    }
+}
